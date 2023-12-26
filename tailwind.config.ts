9e0de8207e271f68bcfbcc01c17de7ed255199f2
Marco Túlio Rodrigues da Silveira <marcosdevprofessional@gmail.com>
2023-12-26 12:02:02 -0300

    v23.1.0

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr'
+      },
+
+      maxWidth: {
+        app: '700px'
+      },
+      colors: {
+        marcao: '#8257e6'
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
